89a548b1160369d91e78b278532695111e9411d0
LinuxIsExcellent <842272994@qq.com>
2022-01-26 23:34:06 +0800

    add todo lists

---
+++ Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,159 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[18];
+    char stringdata0[217];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 15), // "OnServerConnect"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 13), // "OnSocketError"
+QT_MOC_LITERAL(4, 42, 28), // "QAbstractSocket::SocketError"
+QT_MOC_LITERAL(5, 71, 5), // "error"
+QT_MOC_LITERAL(6, 77, 15), // "OnServerMsgRecv"
+QT_MOC_LITERAL(7, 93, 14), // "OnSndServerMsg"
+QT_MOC_LITERAL(8, 108, 7), // "nSystem"
+QT_MOC_LITERAL(9, 116, 4), // "nCmd"
+QT_MOC_LITERAL(10, 121, 11), // "std::string"
+QT_MOC_LITERAL(11, 133, 4), // "data"
+QT_MOC_LITERAL(12, 138, 21), // "OnClickTreeWidgetItem"
+QT_MOC_LITERAL(13, 160, 16), // "QTreeWidgetItem*"
+QT_MOC_LITERAL(14, 177, 4), // "item"
+QT_MOC_LITERAL(15, 182, 6), // "column"
+QT_MOC_LITERAL(16, 189, 20), // "OnCloseTabWidgetSlot"
+QT_MOC_LITERAL(17, 210, 6) // "nIndex"
+
+    },
+    "MainWindow\0OnServerConnect\0\0OnSocketError\0"
+    "QAbstractSocket::SocketError\0error\0"
+    "OnServerMsgRecv\0OnSndServerMsg\0nSystem\0"
+    "nCmd\0std::string\0data\0OnClickTreeWidgetItem\0"
+    "QTreeWidgetItem*\0item\0column\0"
+    "OnCloseTabWidgetSlot\0nIndex"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x0a /* Public */,
+       3,    1,   45,    2, 0x0a /* Public */,
+       6,    0,   48,    2, 0x0a /* Public */,
+       7,    3,   49,    2, 0x0a /* Public */,
+      12,    2,   56,    2, 0x0a /* Public */,
+      16,    1,   61,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::UShort, QMetaType::UShort, 0x80000000 | 10,    8,    9,   11,
+    QMetaType::Void, 0x80000000 | 13, QMetaType::Int,   14,   15,
+    QMetaType::Void, QMetaType::Int,   17,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->OnServerConnect(); break;
+        case 1: _t->OnSocketError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
+        case 2: _t->OnServerMsgRecv(); break;
+        case 3: _t->OnSndServerMsg((*reinterpret_cast< quint16(*)>(_a[1])),(*reinterpret_cast< quint16(*)>(_a[2])),(*reinterpret_cast< std::string(*)>(_a[3]))); break;
+        case 4: _t->OnClickTreeWidgetItem((*reinterpret_cast< QTreeWidgetItem*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 5: _t->OnCloseTabWidgetSlot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
